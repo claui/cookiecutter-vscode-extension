ac4fa6354c94b7c2f315238a851d516d40d080ea
Claudia Pellegrino <claui@users.noreply.github.com>
2022-08-13 14:56:13 +0200

    Make error message easier to read

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,22 +4,26 @@ import subprocess
 shutil.rmtree('licenses')
 
 {%- if cookiecutter.install_dependencies_now == "y" %}
+def print_formatted(message: str) -> None:
+    width = max((len(line) for line in message))
+    print('', width * '-', *message, width * '-', sep='\n')
+
 commands = [
     'yarn set version {{ cookiecutter.yarn_version }}',
     'yarn install',
 ]
+
 try:
     for command in commands:
         subprocess.run(command, check=True, shell=True)
 except subprocess.CalledProcessError as e:
-    print(
+    print_formatted([
         f'Yarn failed with exit code {e.returncode}.',
-        'Fix any issues, then go to the '
-        '{{ cookiecutter.project_slug }} directory and re-run:',
+        'Go to the {{ cookiecutter.project_slug }}'
+            ' directory and re-run:',
         *[
             f'    {command}'
             for command in ['nvm use'] + commands
         ],
-        sep='\n',
-    )
+    ])
 {%- endif %}
